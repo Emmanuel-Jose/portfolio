3ae01e0ee6878f125ea6db425489accbbf2d819f
Jose Bayona <bayonaj368@gmail.com>
2022-12-13 14:14:31 -0600

    update left and right pading in home page text

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
 
         <section className="w-full h-screen snap-start bg-pink flex flex-col justify-center items-start" id='home'>
 
-            <div className="w-auto h-screen flex flex-col justify-center items-center pl-4 md:pl-10">
+            <div className="w-auto h-screen flex flex-col justify-center items-center px-4 md:px-10">
 
                 <h1 className="text-3xl md:text-8xl font-bold p-5 bg-white shadow-home-titles">Emmanuel Bayona</h1>
 
@@ -16,6 +16,6 @@ export const Home = () => {
             
 
         </section>
-        
+
     )
 }
